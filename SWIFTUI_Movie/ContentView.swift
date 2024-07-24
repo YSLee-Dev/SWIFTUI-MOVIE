1959b2f353e9185f6aba252d1cef3d4193e3c11a
YSLee-Dev <ysleedev01@gmail.com>
2024-07-24 14:00:08 +0900

    First Commit

---
+++ SWIFTUI_Movie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SWIFTUI_Movie
+//
+//  Created by 이윤수 on 7/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SWIFTUI_Movie/SWIFTUI_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_MovieApp.swift
+//  SWIFTUI_Movie
+//
+//  Created by 이윤수 on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
