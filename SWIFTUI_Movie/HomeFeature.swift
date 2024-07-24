fdb3cc568ed527a18ced9c848dc54a2e81eef98e
YSLee-Dev <ysleedev01@gmail.com>
2024-07-24 14:08:10 +0900

    Feat(Home): HomeView, HomeFeature 파일 추가

---
+++ SWIFTUI_Movie/HomeFeature.swift
@@ -0,0 +1,8 @@
+//
+//  HomeFeature.swift
+//  SWIFTUI_Movie
+//
+//  Created by 이윤수 on 7/24/24.
+//
+
+import Foundation
---
+++ SWIFTUI_Movie/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SWIFTUI_Movie
+//
+//  Created by 이윤수 on 7/24/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
