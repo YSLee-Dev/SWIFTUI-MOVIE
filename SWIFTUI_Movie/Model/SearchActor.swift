a485a532705b9bc17663e649255669348580e736
YSLee-Dev <ysleedev01@gmail.com>
2024-08-04 22:57:59 +0900

    Fix(Detail(Kobis)): 배우 검색 Model 수정

--- SWIFTUI_Movie/Model/SearchActor.swift
+++ SWIFTUI_Movie/Model/SearchActor.swift
@@ -1,5 +1,5 @@
 //
-//  SearchActorList.swift
+//  SearchActor.swift
 //  SWIFTUI_Movie
 //
 //  Created by 이윤수 on 8/4/24.
@@ -7,8 +7,30 @@
 
 import Foundation
 
-struct SearchActorList: Decodable, Equatable, Hashable {
+struct SearchActor: Decodable, Equatable, Hashable {
+    let actorList: SearchActorList
     
+    enum CodingKeys: String, CodingKey {
+        case actorList = "peopleListResult"
+    }
 }
 
-struct SearchActorList
+struct SearchActorList: Decodable, Equatable, Hashable {
+    let actorDetailList: [SearchActorDetail]
+    
+    enum CodingKeys: String, CodingKey {
+        case actorDetailList = "peopleList"
+    }
+}
+
+struct SearchActorDetail: Decodable, Equatable, Hashable {
+    let name: String
+    let id: String
+    let filmoList: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "peopleNm"
+        case id = "peopleCd"
+        case filmoList = "filmoNames"
+    }
+}
