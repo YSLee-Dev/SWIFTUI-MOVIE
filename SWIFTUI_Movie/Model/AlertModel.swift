0be2a6a7c6b60377a6a27004e5ccd0f8a1fd6678
YSLee-Dev <ysleedev01@gmail.com>
2024-08-06 13:56:05 +0900

    Feat(Network): 네트워크 에러처리를 전담하는 Handler 추가, Alert Model 추가

---
+++ SWIFTUI_Movie/Model/AlertModel.swift
@@ -0,0 +1,13 @@
+//
+//  AletModel.swift
+//  SWIFTUI_Movie
+//
+//  Created by 이윤수 on 8/6/24.
+//
+
+import Foundation
+
+struct AletModel: Equatable {
+    let title: String
+    let msg: String
+}
---
+++ SWIFTUI_Movie/Network/ErrorHandler.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorManager.swift
+//  SWIFTUI_Movie
+//
+//  Created by 이윤수 on 8/6/24.
+//
+
+import Foundation
+
+struct ErrorManager {
+    private init() {}
+    
+    static func handle(error: Error) {
+        
+    }
+}
