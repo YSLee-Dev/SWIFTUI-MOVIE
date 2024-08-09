7db22f954d0eb7c40ec7e8b0425bf2cba7284127
YSLee-Dev <ysleedev01@gmail.com>
2024-08-09 16:52:36 +0900

    Feat(Search): View, Reducer 구성 및 결과 표출

--- SWIFTUI_Movie/Presentation/Search/SearchFeature.swift
+++ SWIFTUI_Movie/Presentation/Search/SearchFeature.swift
@@ -10,18 +10,52 @@ import ComposableArchitecture
 
 @Reducer
 struct SearchFeature: Reducer {
+    @Dependency (\.kobisManager) var kobisManager
+    @Dependency (\.dismiss) var dismiss
+    
     @ObservableState
     struct State: Equatable {
-        
+        var searchQuery: String = ""
+        var searchResult: [MovieSearchDetail] = []
+        var searchType: MovieSearchType = .movieName
     }
     
-    struct Action: Equatable {
-        
+    enum Action:  BindableAction, Equatable {
+        case binding(BindingAction<State>)
+        case movieSearchSuccess([MovieSearchDetail])
+        case movieTapped(String)
+        case backBtnTapped
     }
     
     var body: some Reducer<State, Action> {
+        BindingReducer()
+        
         Reduce { state, action in
-            return .none
+            switch action {
+            case .binding(\.searchQuery):
+                let nowState = state
+                
+                if state.searchQuery.isEmpty {return .none}
+                
+                return .run(operation: { send in
+                    let data = try await self.kobisManager.movieSearchRequest(query: nowState.searchQuery, movieSearchType: nowState.searchType)
+                    await send(.movieSearchSuccess(data))
+                }) { error, send in
+                    // TODO: Error 처리
+                }
+                
+            case .movieSearchSuccess(let data):
+                state.searchResult = data
+                return .none
+                
+            case .backBtnTapped:
+                return .run { _ in
+                    await self.dismiss()
+                }
+                
+            default:
+                return .none
+            }
         }
     }
 }
--- SWIFTUI_Movie/Presentation/Search/SearchView.swift
+++ SWIFTUI_Movie/Presentation/Search/SearchView.swift
@@ -7,12 +7,66 @@
 
 import SwiftUI
 import ComposableArchitecture
+import Kingfisher
 
 struct SearchView: View {
     @State var store: StoreOf<SearchFeature>
     
     var body: some View {
-        Text("")
+        VStack(alignment: .leading) {
+            LargeNavigationBar(title: "검색") {
+                self.store.send(.backBtnTapped)
+            }
+            
+            Picker(selection: self.$store.searchType, label: Text("종류 선택하기")) {
+                ForEach(MovieSearchType.allCases, id: \.self) {
+                    Text($0.rawValue).tag($0)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.bottom, 20)
+            
+            TextField(text: self.$store.searchQuery) {
+                Text(self.store.searchType.placeHolderText)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .padding(.leading, 20)
+            }
+            .padding(.leading, 20)
+            .frame(height: 50)
+            .background {
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(Color.gray.opacity(0.1))
+            }
+            .padding(.bottom, 20)
+            
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    ForEach(Array(zip(self.store.searchResult.indices, self.store.searchResult)), id: \.0) { index, data in
+                        HStack(alignment: .center) {
+                            // 임시 포스터
+                            RoundedRectangle(cornerRadius: 15)
+                                .foregroundColor(.init(uiColor: .systemGray4))
+                                .frame(width: 50, height: 75)
+                                .padding(.trailing, 10)
+                            
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(data.movieName)
+                                    .font(.system(size: 18, weight: .bold))
+                                
+                                Text("\(data.openDate) | \(data.nation)")
+                                    .font(.system(size: 14))
+                            }
+                        }
+                        .frame(height: 90)
+                        .onTapGesture(perform: {
+                            self.store.send(.movieTapped(data.movieID))
+                        })
+                    }
+                }
+            }
+        }
+        .padding(EdgeInsets(top: 30, leading: 20, bottom: 0, trailing: 20))
     }
 }
 
