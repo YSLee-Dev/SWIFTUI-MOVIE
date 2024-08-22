5e951bec38fa2423362757ec74f733bdb85bbba6
YSLee-Dev <ysleedev01@gmail.com>
2024-08-22 10:32:16 +0900

    Feat(Common): 커스텀 팝업 View, Feature 추가

---
+++ SWIFTUI_Movie/Presentation/Common/Popup/PopupView.swift
@@ -0,0 +1,18 @@
+//
+//  PopupView.swift
+//  SWIFTUI_Movie
+//
+//  Created by 이윤수 on 8/22/24.
+//
+
+import SwiftUI
+
+struct PopupView: View {
+    var body: some View {
+        Text("1")
+    }
+}
+
+#Preview {
+    PopupView()
+}
