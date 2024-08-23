811fe33dc87f5f634d430ba8c3ff5f2e39d3e52d
YSLee-Dev <ysleedev01@gmail.com>
2024-08-23 11:28:37 +0900

    Feat(TotalMemo): 메모가 없을 때 나오는 라벨 추가

--- SWIFTUI_Movie/Presentation/TotalMemo/TotalMemoView.swift
+++ SWIFTUI_Movie/Presentation/TotalMemo/TotalMemoView.swift
@@ -21,41 +21,53 @@ struct TotalMemoView: View {
             
             ScrollView {
                 LazyVStack(alignment: .center,  spacing: 20) {
-                    ForEach(Array(zip(self.store.nowMemoList.indices, self.store.nowMemoList)), id: \.0) { index, data in
+                    if self.store.nowMemoList.isEmpty {
                         HStack {
-                            KFImage(data.thumnail)
-                                .resizable()
-                                .placeholder {
-                                    Rectangle()
-                                        .fill(Color.gray.opacity(0.1))
-                                }
-                                .clipShape(RoundedRectangle(cornerRadius: 15))
-                                .frame(width: 100, height: 150)
-                                .padding(.trailing, 10)
-                            
-                            VStack(alignment: .leading, spacing: 10) {
-                                Text(data.movieTitle)
-                                    .font(.system(size: 22, weight: .heavy))
-                                
-                                Text(data.movieNote)
-                                    .font(.callout)
-                                    .lineLimit(3)
-                            }
-                            
                             Spacer()
-                            
-                            Image(systemName: "chevron.forward")
-                                .padding(.trailing, 10)
+                            Text("저장된 메모가 없어요.\n영화 상세페이지에서 메모를 등록할 수 있어요.")
+                                .font(.title3)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.gray)
+                            Spacer()
                         }
-                        .padding(10)
-                        .background {
-                            Rectangle()
-                                .fill(Color.gray.opacity(0.1))
-                                .clipShape(RoundedRectangle(cornerRadius: 15))
-                        }
-                        .padding(.horizontal, 20)
-                        .onTapGesture {
-                            self.store.send(.memoTapped(index))
+                        .padding(.top, 20)
+                    } else {
+                        ForEach(Array(zip(self.store.nowMemoList.indices, self.store.nowMemoList)), id: \.0) { index, data in
+                            HStack {
+                                KFImage(data.thumnail)
+                                    .resizable()
+                                    .placeholder {
+                                        Rectangle()
+                                            .fill(Color.gray.opacity(0.1))
+                                    }
+                                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                                    .frame(width: 100, height: 150)
+                                    .padding(.trailing, 10)
+                                
+                                VStack(alignment: .leading, spacing: 10) {
+                                    Text(data.movieTitle)
+                                        .font(.system(size: 22, weight: .heavy))
+                                    
+                                    Text(data.movieNote)
+                                        .font(.callout)
+                                        .lineLimit(3)
+                                }
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.forward")
+                                    .padding(.trailing, 10)
+                            }
+                            .padding(10)
+                            .background {
+                                Rectangle()
+                                    .fill(Color.gray.opacity(0.1))
+                                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                            }
+                            .padding(.horizontal, 20)
+                            .onTapGesture {
+                                self.store.send(.memoTapped(index))
+                            }
                         }
                     }
                 }
